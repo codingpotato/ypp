db7f0fc51cdc897134dcd6011240a23e87117337
Yongwu <codingpotato@gmail.com>
2018-09-09 21:10:08 +0800

    Fix syscall and other c file not link issue

--- examples/minimal/main.cpp
+++ examples/minimal/main.cpp
@@ -1,29 +1,14 @@
 #include <cstdio>
 #include "board_init.h"
 #include "gpio.h"
-#include "system.h"
-
-struct A {
-  A() {
-    b = 1;
-  }
-  int b = 0;
-};
-
-A a;
+#include "stm32f7xx_nucleo_144.h"
 
 int main(void) {
   board_init();
 
-  if (on_psp()) {
-    gpio_toggle_led(2);
-  }
-
-  print_LR();
-
-  while (1) {
+  while (true) {
     printf("toggle led\r\n");
     gpio_toggle_led(1);
-    sleep(100);
+    sleep(1000);
   }
 }
