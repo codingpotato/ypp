538271f12d609cb74127cbd7f9d2fa8d546df236
Yongwu <codingpotato@gmail.com>
2018-09-09 11:55:20 +0800

    Test psp in minimal example

--- examples/minimal/main.cpp
+++ examples/minimal/main.cpp
@@ -1,52 +1,27 @@
 #include <cstdio>
 #include "board_init.h"
 #include "gpio.h"
+#include "system.h"
 
-/*int main(void) {
+struct A {
+  A() {
+    b = 1;
+  }
+  int b = 0;
+};
+
+A a;
+
+int main(void) {
   board_init();
+
+  if (on_psp()) {
+    gpio_toggle_led(2);
+  }
 
   while (1) {
     printf("toggle led\r\n");
     gpio_toggle_led(1);
     sleep(100);
   }
-}*/
-
-#include <stddef.h>
-#include <stdio.h>
-
-using fcontext_t = void *;
-
-struct transfer_t {
-  fcontext_t fctx;
-  void *data;
-};
-
-extern "C" transfer_t jump_fcontext(fcontext_t to, void *vp);
-extern "C" fcontext_t make_fcontext(void *sp, size_t size,
-                                    void (*fn)(transfer_t));
-
-void f1(transfer_t t) {
-  printf("here\n");
-  gpio_set_led(1);
-  jump_fcontext(t.fctx, nullptr);
-}
-
-constexpr int STACK_SIZE = 1024;
-char stack[STACK_SIZE];
-
-int main() {
-  board_init();
-
-  printf("first\n");
-  gpio_set_led(0);
-
-  fcontext_t ctx = make_fcontext(stack + STACK_SIZE, STACK_SIZE, f1);
-
-  jump_fcontext(ctx, nullptr);
-
-  printf("second\n");
-  gpio_set_led(2);
-
-  return 0;
 }
